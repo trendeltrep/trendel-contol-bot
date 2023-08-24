4b60c764ffbcc9c0cc2c303ad4d4c3921948f52c
Maksym <maksym.krykukha@nure.ua>
2023-08-24 10:07:18 +0300

    feat: added function to open and close browser

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import logging
 import os
+import platform
 import subprocess
 import pyautogui
 from dotenv import load_dotenv
@@ -15,6 +16,7 @@ logging.basicConfig(level=logging.INFO)
 
 # Your bot's API token (replace 'API_TOKEN' with your actual API token)
 API_TOKEN = os.getenv("API_TOKEN")
+PATH_TO_STEAM = os.getenv("PATH_TO_STEAM")
 
 # Initialize bot and dispatcher
 bot = Bot(token=API_TOKEN)
@@ -39,9 +41,9 @@ async def cmd_start(message: types.Message):
 @dp.message_handler(commands=["steam_start"])
 async def steam_start(message: types.Message):
     if is_admin(message.from_user.id):
-        steam_exe = r"D:\steam\Steam.exe"  # Adjust the path to your Steam executable
+        # Adjust the path to your Steam executable
         await message.answer("Opening...")
-        subprocess.Popen([steam_exe])
+        subprocess.Popen([PATH_TO_STEAM])
         await message.answer("Done")
 
     else:
@@ -52,9 +54,14 @@ async def steam_start(message: types.Message):
 @dp.message_handler(commands=["steam_close"])
 async def steam_close(message: types.Message):
     if is_admin(message.from_user.id):
-        await message.answer("Closing...")
-        subprocess.run(["taskkill", "/F", "/IM", "steam.exe"])
-        await message.answer("Done")
+        if platform.system() == "Windows":
+            subprocess.run(["taskkill", "/F", "/IM", "steam.exe"])
+        elif platform.system() == "Darwin":
+            subprocess.run(["pkill", "-x", "Steam"])
+        elif platform.system() == "Linux":
+            subprocess.run(["pkill", "steam"])
+        else:
+            print("Unsupported operating system")
     else:
         send_unauthorized_message(message)
 
@@ -72,11 +79,43 @@ async def take_screenshot(message: types.Message):
         send_unauthorized_message(message)
 
 
+# Opening a browser for searching information
+@dp.message_handler(commands=["chrome"])
+async def chrome_open(message: types.Message):
+    if is_admin(message.from_user.id):
+        query = " ".join(message.text.split()[1:])  # Extract the search query
+        search_url = f"https://www.google.com/search?q={query}"
+
+        # Open Google Chrome with the search URL
+        subprocess.Popen(
+            ["chrome", search_url]
+        )  # Adjust this command based on your system
+
+        await message.answer(f"Searching for '{query}' in Google Chrome...")
+    else:
+        send_unauthorized_message(message)
+
+
+# Closing browser
+@dp.message_handler(commands=["chrome_close"])
+def chrome_close():
+    if platform.system() == "Windows":
+        subprocess.run(["taskkill", "/F", "/IM", "chrome.exe"])
+    elif platform.system() == "Darwin":
+        subprocess.run(["pkill", "-x", "Google Chrome"])
+    elif platform.system() == "Linux":
+        subprocess.run(["pkill", "chrome"])
+    else:
+        print("Unsupported operating system")
+
+
 # Register a text message handler
 @dp.message_handler(content_types=types.ContentTypes.TEXT)
-async def echo_message(message: types.Message):
+async def test(message: types.Message):
     if is_admin(message.from_user.id):
-        await message.answer(f"You said: {message.text}")
+        words = message.text.split()[1:]  # Skip the command itself
+        for word in words:
+            await message.answer(word)
     else:
         send_unauthorized_message(message)
 

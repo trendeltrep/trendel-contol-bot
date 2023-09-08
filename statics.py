27047ead854fce1576395b91488e4c15f920b830
Maksym <maksym.krykukha@nure.ua>
2023-09-08 17:43:13 +0300

    fix

--- statics.py
+++ statics.py
@@ -7,6 +7,7 @@ file_name = "statics.json"
 def load():
     try:
         with open(file_name, "r") as file:
+            commands = loading.get_commands()
             return json.load(file)
     except FileNotFoundError:
         with open(file_name, "w") as file:
@@ -31,4 +32,6 @@ def save_data():
 # Resetting data to default
 def reset_data():
     with open(file_name, "w") as file:
-        json.dump(data, file)
+        data = loading.get_commands()
+        data_used = {used: 0 for used in data}
+        json.dump(data_used, file)
